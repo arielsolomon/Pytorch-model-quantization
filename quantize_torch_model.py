22b55da5fa07c6b35432681922075f998d8b8bb5
Ariel Solomon <46421668+arielsolomon@users.noreply.github.com>
2022-05-24 15:25:26 +0300

    Update quantize_torch_model.py

--- quantize_torch_model.py
+++ quantize_torch_model.py
@@ -4,37 +4,38 @@ import pathlib
 import onnx
 import export_unparsed as ex
 
+"""
+The process includes .pt-->.onnx-->.tflite-->quantization-->.onnx (to be used directly)
+"""
 
-# first define model class
-#loading model
-root = '/Data/Signals/Sat_proj/yolov5/'
-model_name = 'epoch2275_best_SGD_square_1.pt'
+#loading torch model
+root = 'folder of your model'
+model_name = 'model.pt'
 model_path = root+ model_name
 types = ['tflite', 'onnx']
 onnx_model1 = model_path.replace('pt', 'onnx')
-ex.main()
+#to convert model to onnx
+ex.main() # I found it works better for me to directly add parameters in export_unparsed.py
+          # you can parse it as you wish
+#Load the onnx model  
 onnx_model = onnx.load(onnx_model1)
+
 #conversion to tflite model (tflite model needed to quantization)
 tf_rep = prepare(onnx_model)
-
 tf_rep.export_graph('/'.join(model_path.split('/')[:-1])+'/tf_model_pb')
 tf_model = model_path.replace('pt', 'pb')
+
 # quantize tf model
 converter = tf.lite.TFLiteConverter.from_saved_model(root+'tf_model_pb/')
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_quant_model = converter.convert()
+
 # save quantized model
 tflite_models_dir = pathlib.Path(root)
 tflite_model_quant_file = tflite_models_dir/model_name.replace('pt', 'tflite')
 tflite_model_quant_file.write_bytes(tflite_quant_model)
 
-#back path to pytorch model
+#use cli to convert the quantized tflite file to onnx model
 
-tflite_path = root+ model_name.replace('pt', 'tflite')
-
-# onnx_quantized_export_model_path = root+ model_name.replace('pt', 'onnx')
-"""
-using script command it did not work and using cli it does work
-python3.8 -m tf2onnx.convert --tflite 'tflite_path --output onnx_quantized_export_model_path --opset 13
-now you have quantized model back in form of onnx
-"""
+python3.8 -m tf2onnx.convert --tflite 'tflite_quantized_model_path --output onnx_quantized_model_path --opset 13
+# now you have quantized model in form of onnx
